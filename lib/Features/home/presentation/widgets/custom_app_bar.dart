1eb43ea6d6ebc2858c727213b20edd0e5b93ee89
a7medjamal <ahmedjmal20003@gmail.com>
2025-07-11 20:31:57 +0300

    feat(home): add custom app bar and home view body widgets

---
+++ lib/Features/home/presentation/widgets/custom_app_bar.dart
@@ -0,0 +1,28 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Row(
+        children: [
+          Image.asset(AssetsData.booklyLogo, height: 18),
+          const Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 24,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
