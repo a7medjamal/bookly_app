2689e879298c07bebd77e568b39c6c7a3d4a7179
a7medjamal <ahmedjmal20003@gmail.com>
2025-07-11 20:52:19 +0300

    feat(home): add custom list view item with test image

--- lib/Features/home/presentation/widgets/home_view_body.dart
+++ lib/Features/home/presentation/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/Features/home/presentation/widgets/custom_app_bar.dart';
+import 'package:bookly_app/Features/home/presentation/widgets/custom_list_view_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,6 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [CustomAppBar()]);
+    return Column(children: [CustomAppBar(), CustomListViewItem()]);
   }
 }
--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,4 @@
 class AssetsData {
   static const String booklyLogo = 'assets/images/bookly_logo.png';
+  static const String testImage = 'assets/images/test_image.png';
 }
