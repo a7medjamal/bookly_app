78b2392368bd43777be4d2e26a5bae2300d3a847
a7medjamal <ahmedjmal20003@gmail.com>
2025-07-13 06:02:59 +0300

    feat(ui): add google fonts and style constants for text themes

--- lib/Features/home/presentation/widgets/home_view_body.dart
+++ lib/Features/home/presentation/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:bookly_app/Features/home/presentation/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/widgets/featured_books_list_view.dart';
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -17,7 +17,41 @@ class HomeViewBody extends StatelessWidget {
           CustomAppBar(),
           FeaturedBooksListView(),
           const SizedBox(height: 40),
-          Text('Best Seller', style: Styles.titleMedium),
+          Text('Best Seller', style: Styles.textStyle18),
+          const SizedBox(height: 16),
+          const BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.7 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.testImage),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Column(children: [
+            
+            ],
+          ),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -13,7 +14,10 @@ class BooklyApp extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: AppRouter.router,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
     );
   }
 }
