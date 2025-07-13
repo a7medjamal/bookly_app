a175239987a005b61c684e4fbdf3c9c10e50b9a9
a7medjamal <ahmedjmal20003@gmail.com>
2025-07-13 06:20:08 +0300

    refactor(home): move best seller item to separate file and adjust styles

--- lib/Features/home/presentation/widgets/home_view_body.dart
+++ lib/Features/home/presentation/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
+import 'package:bookly_app/Features/home/presentation/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/Features/home/presentation/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/widgets/featured_books_list_view.dart';
-import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +10,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -18,40 +18,8 @@ class HomeViewBody extends StatelessWidget {
           FeaturedBooksListView(),
           const SizedBox(height: 40),
           Text('Best Seller', style: Styles.textStyle18),
-          const SizedBox(height: 16),
+          const SizedBox(height: 20),
           const BestSellerListViewItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 130,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.7 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.red,
-                image: const DecorationImage(
-                  image: AssetImage(AssetsData.testImage),
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-          ),
-          Column(children: [
-            
-            ],
-          ),
         ],
       ),
     );
