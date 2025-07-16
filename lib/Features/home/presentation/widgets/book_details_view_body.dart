16917550fb8b7d700621e1e0ff9e53b16b22c3eb
a7medjamal <ahmedjmal20003@gmail.com>
2025-07-16 13:45:41 +0300

    refactor(home): wrap book details view in scrollable layout

--- lib/Features/home/presentation/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/widgets/book_details_view_body.dart
@@ -12,45 +12,94 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
-      child: Column(
-        children: [
-          CustomBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.25),
-            child: CustomBookImage(),
-          ),
-          const SizedBox(height: 33),
-          Text('The Jungle Book', style: Styles.textStyle30),
-          const SizedBox(height: 4),
-          Opacity(
-            opacity: 0.7,
-            child: Text(
-              'Rudyard Kipling',
-              style: Styles.textStyle18.copyWith(
-                fontWeight: FontWeight.w500,
-                fontStyle: FontStyle.italic,
-              ),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
+            child: Column(
+              children: [
+                CustomBookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * 0.25),
+                  child: CustomBookImage(),
+                ),
+                const SizedBox(height: 33),
+                Text('The Jungle Book', style: Styles.textStyle30),
+                const SizedBox(height: 4),
+                Opacity(
+                  opacity: 0.7,
+                  child: Text(
+                    'Rudyard Kipling',
+                    style: Styles.textStyle18.copyWith(
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const BookRating(mainAxisAlignment: MainAxisAlignment.center),
+                const SizedBox(height: 37),
+                BooksAction(),
+                Expanded(child: const SizedBox(height: 40)),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.textStyle14.copyWith(
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const SimilarBooksListView(),
+                const SizedBox(height: 25),
+              ],
             ),
           ),
-          const SizedBox(height: 10),
-          const BookRating(mainAxisAlignment: MainAxisAlignment.center),
-          const SizedBox(height: 37),
-          BooksAction(),
-          const SizedBox(height: 40),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w900),
-            ),
-          ),
-          const SizedBox(height: 16),
-          const SimilarBooksListView(),
-          const SizedBox(height: 25),
-        ],
-      ),
+        ),
+      ],
     );
+    // return Padding(
+    //   padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
+    //   child: Column(
+    //     children: [
+    //       CustomBookDetailsAppBar(),
+    //       Padding(
+    //         padding: EdgeInsets.symmetric(horizontal: width * 0.25),
+    //         child: CustomBookImage(),
+    //       ),
+    //       const SizedBox(height: 33),
+    //       Text('The Jungle Book', style: Styles.textStyle30),
+    //       const SizedBox(height: 4),
+    //       Opacity(
+    //         opacity: 0.7,
+    //         child: Text(
+    //           'Rudyard Kipling',
+    //           style: Styles.textStyle18.copyWith(
+    //             fontWeight: FontWeight.w500,
+    //             fontStyle: FontStyle.italic,
+    //           ),
+    //         ),
+    //       ),
+    //       const SizedBox(height: 10),
+    //       const BookRating(mainAxisAlignment: MainAxisAlignment.center),
+    //       const SizedBox(height: 37),
+    //       BooksAction(),
+    //       Expanded(child: const SizedBox(height: 40)),
+    //       Align(
+    //         alignment: Alignment.centerLeft,
+    //         child: Text(
+    //           'You can also like',
+    //           style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w900),
+    //         ),
+    //       ),
+    //       const SizedBox(height: 16),
+    //       const SimilarBooksListView(),
+    //       const SizedBox(height: 25),
+    //     ],
+    //   ),
+    // );
   }
 }
