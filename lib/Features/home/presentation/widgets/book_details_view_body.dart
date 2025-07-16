2803dc5144c2fdcb65ebb8ec1fae8cd7b062a0e4
a7medjamal <ahmedjmal20003@gmail.com>
2025-07-16 13:40:28 +0300

    feat(home): add similar books list view to book details

--- lib/Features/home/presentation/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/widgets/book_details_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/Features/home/presentation/widgets/book_rating.dart';
 import 'package:bookly_app/Features/home/presentation/widgets/books_action.dart';
 import 'package:bookly_app/Features/home/presentation/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/widgets/custom_book_image_item.dart';
+import 'package:bookly_app/Features/home/presentation/widgets/similar_books_list_view.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -37,6 +38,17 @@ class BookDetailsViewBody extends StatelessWidget {
           const BookRating(mainAxisAlignment: MainAxisAlignment.center),
           const SizedBox(height: 37),
           BooksAction(),
+          const SizedBox(height: 40),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You can also like',
+              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w900),
+            ),
+          ),
+          const SizedBox(height: 16),
+          const SimilarBooksListView(),
+          const SizedBox(height: 25),
         ],
       ),
     );
