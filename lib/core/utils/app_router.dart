3e58b2bea94a231cfa0475a3d77f10a403d6bae6
a7medjamal <ahmedjmal20003@gmail.com>
2025-07-16 16:04:16 +0300

    feat(search): add search view with text field and navigation
    Implement search functionality including:
    - SearchView and SearchViewBody widgets
    - CustomSearchTextField component
    - Navigation from app bar to search view
    - Update router configuration for search view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,11 +1,13 @@
 import 'package:bookly_app/Features/home/presentation/book_details_view.dart';
 import 'package:bookly_app/Features/home/presentation/home_view.dart';
+import 'package:bookly_app/Features/search/presentation/search_view.dart';
 import 'package:bookly_app/Features/splash/presentation/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
-  static const String kHomeView = '/home';
-  static const String kBookDetailsView = '/bookDetails';
+  static const String kHomeView = '/homeView';
+  static const String kBookDetailsView = '/bookDetailsView';
+  static const String kSearchView = '/searchView';
   static final router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => const SplashView()),
@@ -13,6 +15,10 @@ class AppRouter {
       GoRoute(
         path: kBookDetailsView,
         builder: (context, state) => const BookDetailsView(),
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: (context, state) => const SearchView(),
       ),
     ],
   );
