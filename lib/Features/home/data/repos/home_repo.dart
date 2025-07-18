b5aff231327a135d06242f1953ccd4c03c690b1c
a7medjamal <ahmedjmal20003@gmail.com>
2025-07-18 16:28:35 +0300

    feat(home): add error handling using dartz and Failure class
    - Introduce abstract Failure class for error handling
    - Add dartz dependency for Either type
    - Modify HomeRepo methods to return Either<Failure, List<BookModel>>

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,8 +1,9 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo 
 {
-  Future<List<BookModel>> fetchNewestBooks();
-  Future<List<BookModel>> fetchFeaturedBooks();
-  Future<List<BookModel>> fetchSimilarBooks({required String category});
+  Future<Either<Failure,List<BookModel>>> fetchNewestBooks();
+  Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 }
