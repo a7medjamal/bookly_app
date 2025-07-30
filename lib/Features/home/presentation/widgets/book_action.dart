e125affdb7fb526707cdaf47d119145e9592b94f
a7medjamal <ahmedjmal20003@gmail.com>
2025-07-30 19:07:52 +0300

    refactor(home): extract url launching logic to utils functions
    Move url launching and snackbar logic to separate utility functions to improve code reusability and maintainability. Update book action widget to use these new utility functions.

--- lib/Features/home/presentation/widgets/book_action.dart
+++ lib/Features/home/presentation/widgets/book_action.dart
@@ -1,7 +1,7 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/utils/functions/launch_custom_url.dart';
 import 'package:bookly_app/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class BooksAction extends StatelessWidget {
   const BooksAction({super.key, required this.bookModel});
@@ -27,34 +27,14 @@ class BooksAction extends StatelessWidget {
           Expanded(
             child: CustomButton(
               onPressed: () async {
-                Uri uri = Uri.parse(bookModel.volumeInfo!.previewLink!);
-                if (await launchUrl(uri)) {
-                  await launchUrl(uri);
-                } else {
-                  Uri uri = Uri.parse(bookModel.accessInfo!.webReaderLink!);
-                  if (await canLaunchUrl(uri)) {
-                    if (context.mounted) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('This is just available for preview'),
-                        ),
-                      );
-                    }
-                    await launchUrl(uri);
-                  } else {
-                    if (context.mounted) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Could not launch preview'),
-                        ),
-                      );
-                    }
-                  }
-                }
+                await launchCustomUrl(
+                  bookModel.volumeInfo!.previewLink!,
+                  context,
+                );
               },
               backgroundColor: const Color(0xFFEF8262),
               textColor: Colors.white,
-              text: 'Preview',
+              text: getText(bookModel),
               fontSize: 16,
               borderRadius: BorderRadius.only(
                 topRight: Radius.circular(16),
@@ -65,5 +45,13 @@ class BooksAction extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+String getText(BookModel bookModel) {
+  if (bookModel.volumeInfo!.previewLink != null) {
+    return 'Preview';
+  } else {
+    return 'Not Available';
   }
 }
---
+++ lib/core/utils/functions/launch_custom_url.dart
@@ -0,0 +1,16 @@
+import 'package:bookly_app/core/utils/functions/custom_snack_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+Future<void> launchCustomUrl(String? url, BuildContext context) async {
+  if (url != null) {
+    Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      if (context.mounted) {
+        customSnackBar(context, 'Could not launch url');
+      }
+    }
+  }
+}
